1593c281ebc11dfe8bbf8378dafc2a9e8eb31f3f
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2023-12-02 22:30:39 +0530

    Setting up all day modules.

---
+++ Day24/src/test/scala/problem/NewProblemSpec.scala
@@ -0,0 +1,19 @@
+package problem
+
+import org.scalatest.GivenWhenThen
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class NewProblemSpec extends AnyFlatSpec with GivenWhenThen with Matchers {
+
+  val filePath = "Day2/src/test/resources/input/input.XSCORE.txt"
+
+//  "NewProblem" should "match 2810" in {
+//    Day3Problem.problem1(filePath) should be(2810)
+//  }
+//
+//  it should "match 69110" in {
+//    Day3Problem.problem2(filePath) should be(69110)
+//  }
+}
