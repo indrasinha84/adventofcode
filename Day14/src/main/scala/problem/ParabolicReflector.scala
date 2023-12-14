77e48b641e3066b1749f6a711ec874c16513ef00
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2023-12-14 20:01:09 +0530

    Tuning.

--- Day14/src/main/scala/problem/ParabolicReflector.scala
+++ Day14/src/main/scala/problem/ParabolicReflector.scala
@@ -31,6 +31,70 @@ object ParabolicReflector {
     input
   }
 
+  private def moveCycle(input: ArrayBuffer[ArrayBuffer[Char]]) = {
+    // North
+    for col <- input.head.indices do
+      var boundary = -1
+      for row <- input.indices do
+        if (input(row)(col) == '#') {
+          boundary = row
+        }
+        else if (input(row)(col) == 'O' && boundary + 1 == row) {
+          boundary += 1
+        }
+        else if (input(row)(col) == 'O') {
+          input(boundary + 1)(col) = 'O'
+          input(row)(col) = '.'
+          boundary += 1
+        }
+    // West
+    for row <- input.indices do
+      var boundary = -1
+      for col <- input(row).indices do
+        if (input(row)(col) == '#') {
+          boundary = col
+        }
+        else if (input(row)(col) == 'O' && boundary + 1 == col) {
+          boundary += 1
+        }
+        else if (input(row)(col) == 'O') {
+          input(row)(boundary + 1) = 'O'
+          input(row)(col) = '.'
+          boundary += 1
+        }
+    // South
+    for col <- input.head.indices do
+      var boundary = input.size
+      for row <- input.indices.reverse do
+        if (input(row)(col) == '#') {
+          boundary = row
+        }
+        else if (input(row)(col) == 'O' && boundary - 1 == row) {
+          boundary -= 1
+        }
+        else if (input(row)(col) == 'O') {
+          input(boundary - 1)(col) = 'O'
+          input(row)(col) = '.'
+          boundary -= 1
+        }
+    // East
+    for row <- input.indices do
+      var boundary = input(row).size
+      for col <- input(row).indices.reverse do
+        if (input(row)(col) == '#') {
+          boundary = col
+        }
+        else if (input(row)(col) == 'O' && boundary - 1 == col) {
+          boundary -= 1
+        }
+        else if (input(row)(col) == 'O') {
+          input(row)(boundary - 1) = 'O'
+          input(row)(col) = '.'
+          boundary -= 1
+        }
+    input
+  }
+
   private def moveSouth(input: ArrayBuffer[ArrayBuffer[Char]]) = {
     val reversedInput = input.reverse
     moveNorth(reversedInput).reverse
@@ -76,9 +140,9 @@ object ParabolicReflector {
   def problem2(filePath: String): Int = {
     var input = readFile(filePath)
     for i <- 1 to 1000000000 do
-      println(i)
-      input = moveEast(moveSouth(moveWest(moveNorth(input))))
-    input.map(_.mkString).foreach(println)
+//      println(i)
+      input = moveCycle(input)
+//    input.map(_.mkString).foreach(println)
     var multiplier = input.size
     input.map(_.count(_ == 'O')).map(c => {
       val r = c * multiplier
