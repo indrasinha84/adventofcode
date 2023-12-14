34faf115e9e39aa432b708ddd79164b0fe567a13
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2023-12-15 03:26:02 +0530

    Working solution with cache.

--- Day14/src/main/scala/problem/ParabolicReflector.scala
+++ Day14/src/main/scala/problem/ParabolicReflector.scala
@@ -6,11 +6,10 @@ import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 import scala.util.Using
 
+// TODO Remove ArrayBuffer
 object ParabolicReflector {
 
-  var cache = mutable.Map.empty[String, Array[Array[Char]]]
-  var westCache = mutable.Map.empty[String, Array[Char]]
-
+  var cache = mutable.Map.empty[String, (String, Array[Array[Char]])]
 
   private def readFile(filePath: String) = {
     ArrayBuffer(Using(Source.fromFile(filePath)) { file =>
@@ -45,24 +44,12 @@ object ParabolicReflector {
   }
 
   @tailrec
-  private def moveCycle(rowSize: Int, columnSize: Int, cnt: Int, inputParam: Array[Array[Char]]): Array[Array[Char]] = {
-    if (cnt % 10000000 == 0) {
-      println(s"Result for cnt $cnt = ${
-        var multiplier = inputParam.length
-        inputParam.map(_.count(_ == 'O')).map(c => {
-          val r = c * multiplier
-          multiplier -= 1
-          r
-        }).sum
-
-      }")
-    }
+  private def moveCycle(rowSize: Int, columnSize: Int, cnt: Int, inputParam: (String, Array[Array[Char]])): Array[Array[Char]] = {
     if (cnt == 0)
-      inputParam
+      inputParam._2
     else {
-      val old = inputParam.map(_.mkString).mkString
-      moveCycle(rowSize, columnSize, cnt - 1, cache.getOrElseUpdate(old, {
-        val input = ArrayBuffer(inputParam.map(a => ArrayBuffer(a: _*)): _*)
+      moveCycle(rowSize, columnSize, cnt - 1, cache.getOrElseUpdate(inputParam._1, {
+        val input = ArrayBuffer(inputParam._2.map(a => ArrayBuffer(a: _*)): _*)
         // North
         for col <- 0 until columnSize do
           var boundary = -1
@@ -80,19 +67,17 @@ object ParabolicReflector {
         for row <- 0 until rowSize do
           var boundary = -1
           val current = input(row)
-          input(row) = ArrayBuffer(westCache.getOrElseUpdate(current.mkString, {
-            for col <- 0 until columnSize do
-              boundary = current(col) match {
-                case '#' => col
-                case 'O' if boundary + 1 == col => boundary + 1
-                case 'O' =>
-                  current(boundary + 1) = 'O'
-                  current(col) = '.'
-                  boundary + 1
-                case _ => boundary
-              }
-            current.toArray
-          }): _*)
+          for col <- 0 until columnSize do
+            boundary = current(col) match {
+              case '#' => col
+              case 'O' if boundary + 1 == col => boundary + 1
+              case 'O' =>
+                input(row)(boundary + 1) = 'O'
+                input(row)(col) = '.'
+                boundary + 1
+              case _ => boundary
+            }
+          current.toArray
         // South
         for col <- 0 until columnSize do
           var boundary = rowSize
@@ -119,7 +104,7 @@ object ParabolicReflector {
                 boundary - 1
               case _ => boundary
             }
-        input.map(_.toArray).toArray
+        (input.map(_.mkString).mkString, input.map(_.toArray).toArray)
       }))
     }
   }
@@ -138,9 +123,8 @@ object ParabolicReflector {
 
   def problem2(filePath: String): Int = {
     var input = readFile2(filePath)
-    input = moveCycle(input.size, input.head.size, 1000000000, input)
-    input.map(_.mkString).foreach(println)
-    var multiplier = input.size
+    input = moveCycle(input.length, input.head.length, 1000000000, (input.map(_.mkString).mkString, input))
+    var multiplier = input.length
     input.map(_.count(_ == 'O')).map(c => {
       val r = c * multiplier
       multiplier -= 1
