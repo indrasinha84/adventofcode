e022c1cf7cbab713d7c409d15c3259e4bf87a4bd
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2023-12-15 03:26:17 +0530

    Working solution with cache.

--- Day14/src/test/scala/problem/ParabolicReflectorSpec.scala
+++ Day14/src/test/scala/problem/ParabolicReflectorSpec.scala
@@ -9,24 +9,13 @@ import java.util.concurrent.TimeUnit
 
 class ParabolicReflectorSpec extends AnyFlatSpec with GivenWhenThen with Matchers {
 
-//  val filePath = "Day14/src/test/resources/input/input.XSCORE.txt"
-  val filePath = "Day14/src/test/resources/input/input.XSCOREsmall.txt"
+  val filePath = "Day14/src/test/resources/input/input.XSCORE.txt"
 
   "ParabolicReflector" should "match 2810" in {
     ParabolicReflector.problem1(filePath) should be(109466)
   }
 
-  it should "match 631" in {
-    val start = System.currentTimeMillis()
-    val result = ParabolicReflector.problem2(filePath)
-    result should not equal null
-    println(s"Result is $result")
-    val duration = System.currentTimeMillis() - start
-    println(s"Completed in ${
-      String.format("%d min, %d sec",
-        TimeUnit.MILLISECONDS.toMinutes(duration),
-        TimeUnit.MILLISECONDS.toSeconds(duration) -
-          TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration))
-      )}")
+  it should "match 94585" in {
+    ParabolicReflector.problem2(filePath) should be(94585)
   }
 }
