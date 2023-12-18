204929ef0a4956e672983f17d2b4b6219ed4e2a7
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2023-12-19 02:28:35 +0530

    Day 17 working but inefficient.

---
+++ Day17/src/main/scala/problem/ClumsyCrucible.scala
@@ -0,0 +1,87 @@
+package problem
+
+import scala.io.Source
+import scala.util.Using
+import problem.Direction.*
+
+import scala.annotation.tailrec
+import scala.collection.immutable.Seq
+import scala.collection.mutable.ArrayBuffer
+
+
+private enum Direction:
+  case Up
+  case Down
+  case Right
+  case Left
+
+final case class Block(row: Int, col: Int, direction: Direction, allowedMoves: Seq[Direction], remainingMoves: Int)
+
+object ClumsyCrucible {
+  type MoveBlock = (ArrayBuffer[ArrayBuffer[Set[(Int, Block)]]], Direction, Block) => Option[Block]
+
+  private def readFile(filePath: String) = Using(Source.fromFile(filePath)) { file =>
+    file
+      .getLines().map(_.toCharArray.map(_.toString.toInt)).toArray
+  }.get
+
+  private def moveBlockFunc(rowLength: Int, colLength: Int)(blockMap: ArrayBuffer[ArrayBuffer[Set[(Int, Block)]]], direction: Direction, currentBlock: Block) = {
+    val remainingMoves = if (currentBlock.direction == direction) currentBlock.remainingMoves - 1 else 2
+    val nextBlock = direction match
+      case Direction.Up => Block(currentBlock.row - 1, currentBlock.col, direction, Seq(Left, Right, Up), remainingMoves)
+      case Direction.Down => Block(currentBlock.row + 1, currentBlock.col, direction, Seq(Left, Right, Down), remainingMoves)
+      case Direction.Right => Block(currentBlock.row, currentBlock.col + 1, direction, Seq(Right, Up, Down), remainingMoves)
+      case Direction.Left => Block(currentBlock.row, currentBlock.col - 1, direction, Seq(Left, Up, Down), remainingMoves)
+    Option.when((remainingMoves >= 0 && nextBlock.row >= 0 && nextBlock.row < rowLength && nextBlock.col >= 0 && nextBlock.col < colLength) &&
+      !(currentBlock.row == rowLength - 1 && currentBlock.col == colLength - 1)
+    )(nextBlock)
+  }
+
+  def findPaths(input: Array[Array[Int]], moveBlock: MoveBlock, paths: Seq[(Int, Seq[(Int, Int)], Block)]): Int = {
+    var shortestPath = Int.MaxValue
+    var results = paths
+    val blockMap = ArrayBuffer.fill(input.length)(ArrayBuffer.fill(input.head.length)(Set.empty[(Int, Block)]))
+    while (results.nonEmpty) {
+      results = results.flatMap(path => {
+        val lastBlock = path._3
+        blockMap(lastBlock.row)(lastBlock.col) = blockMap(lastBlock.row)(lastBlock.col) + ((path._1, lastBlock))
+        val nextMoves = lastBlock.allowedMoves.flatMap(next => moveBlock(blockMap, next, lastBlock) match
+          case Some(value) =>
+            if (value.row == input.length - 1 && value.col == input.head.length - 1) {
+              if ((path._1 + input(value.row)(value.col)) < shortestPath) {
+                shortestPath = path._1 + input(value.row)(value.col)
+              }
+              Seq.empty[(Int, Seq[(Int, Int)], Block)]
+            }
+            else if (blockMap(value.row)(value.col).map(_._1).minOption.getOrElse(Int.MaxValue) > path._1 + input(value.row)(value.col)) {
+              Seq((path._1 + input(value.row)(value.col), path._2 :+ (value.row, value.col), value))
+            }
+            else {
+              Seq.empty[(Int, Seq[(Int, Int)], Block)]
+            }
+          case _ => Seq.empty[(Int, Seq[(Int, Int)], Block)]
+        )
+        nextMoves
+      })
+    }
+    shortestPath
+  }
+
+
+  def problem1(filePath: String): Int = {
+    val input = readFile(filePath)
+    val rowLength = input.length
+    val colLength = input.head.length
+    val moveBlock = moveBlockFunc(rowLength, colLength)
+    findPaths(input, moveBlock, Seq((0, Seq.empty, Block(0, 0, Right, Seq(Right, Down), 2))
+//      , (0, Seq.empty, Block(0, 0, Down, Seq(Right, Down), 2))
+    ))
+  }
+
+  def problem2(filePath: String): Iterator[String] = {
+    Using(Source.fromFile(filePath)) { file =>
+      file
+        .getLines()
+    }.get
+  }
+}
---
+++ Day17/src/test/scala/problem/ClumsyCrucibleSpec.scala
@@ -0,0 +1,19 @@
+package problem
+
+import org.scalatest.GivenWhenThen
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class ClumsyCrucibleSpec extends AnyFlatSpec with GivenWhenThen with Matchers {
+
+  val filePath = "Day17/src/test/resources/input/input.XSCORE.txt"
+
+  "ClumsyCrucible" should "match 102" in {
+    ClumsyCrucible.problem1(filePath) should be(102)
+  }
+
+  it should "match 69110" in {
+    ClumsyCrucible.problem2(filePath) should be(69110)
+  }
+}
