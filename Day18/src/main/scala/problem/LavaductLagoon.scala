bfa380a42f2669e8044b978090cd9570ca4e5b0a
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2023-12-21 15:52:19 +0530

    Day 18 solution both parts.

--- Day18/src/main/scala/problem/LavaductLagoon.scala
+++ Day18/src/main/scala/problem/LavaductLagoon.scala
@@ -1,14 +1,13 @@
 package problem
 
-import scala.collection.mutable
 import scala.io.Source
 import scala.math.abs
 import scala.util.Using
 
 
-final case class DigPlan(direction: Char, steps: Int, color: String)
+final case class DigPlan(direction: Char, steps: Long, color: String)
 
-final case class Block(row: Int, col: Int, color: String)
+final case class Block(row: Long, col: Long, digPlan: DigPlan, endRow: Long, endCol: Long)
 
 object LavaductLagoon {
 
@@ -17,61 +16,54 @@ object LavaductLagoon {
       file
         .getLines()
         .map(_.split(" ") match
-          case Array(direction, steps, color) => DigPlan(direction.head, steps.toInt, color)
+          case Array(direction, steps, color) => DigPlan(direction.head, steps.toLong, color)
         ).toSeq
     }.get
   }
 
-  def buildLavaPool(input: Seq[DigPlan]) = {
-    val start = Block(0, 0, "")
-    val blocks = input.foldLeft(Seq(start))((sum, elem) => {
-      sum ++ (elem.direction match {
-        case 'R' => Seq.range(1, elem.steps + 1).map(s => Block(sum.last.row, sum.last.col + s, elem.color))
-        case 'L' => Seq.range(1, elem.steps + 1).map(s => Block(sum.last.row, sum.last.col - s, elem.color))
-        case 'U' => Seq.range(1, elem.steps + 1).map(s => Block(sum.last.row - s, sum.last.col, elem.color))
-        case 'D' => Seq.range(1, elem.steps + 1).map(s => Block(sum.last.row + s, sum.last.col, elem.color))
-        case _ => Seq.empty[Block]
-      })
+  private def buildLavaPool(input: Seq[DigPlan]): Long = {
+    val blocks = input.foldLeft(Seq.empty[Block])((sum, elem) => {
+      (sum, elem.direction) match {
+        case (Nil, 'R') => Seq(Block(0, 0, elem, 0, elem.steps))
+        case (Nil, 'L') => Seq(Block(0, 0, elem, 0, -elem.steps))
+        case (Nil, 'U') => Seq(Block(0, 0, elem, elem.steps, 0))
+        case (Nil, 'D') => Seq(Block(0, 0, elem, -elem.steps, 0))
+        case (next, 'R') => next :+ Block(next.last.endRow, next.last.endCol, elem, next.last.endRow, next.last.endCol + elem.steps)
+        case (next, 'L') => next :+ Block(next.last.endRow, next.last.endCol, elem, next.last.endRow, next.last.endCol - elem.steps)
+        case (next, 'U') => next :+ Block(next.last.endRow, next.last.endCol, elem, next.last.endRow - elem.steps, next.last.endCol)
+        case (next, 'D') => next :+ Block(next.last.endRow, next.last.endCol, elem, next.last.endRow + elem.steps, next.last.endCol)
+      }
     })
-    val widthOffset = abs(blocks.map(_.col).min)
-    val width = blocks.map(_.col).max + widthOffset + 1
-    val heightOffset = abs(blocks.map(_.row).min)
-    val height = blocks.map(_.row).max + heightOffset + 1
-    val pool = mutable.Seq.fill(height)(mutable.Seq.fill(width)('.'))
-    blocks.foreach(k => pool(k.row + heightOffset)(k.col + widthOffset) = '#')
-    val startingPoint = pool.indices.flatMap(r => pool(r).indices.map(c => (r, c))).find({ case (r, c) =>
-      pool(r)(c) == '#' && pool(r)(c + 1) == '#' && pool(r + 1)(c) == '#' && pool(r + 1)(c + 1) == '.'
-    }).map(p => (p._1 + 1, p._2 + 1)).getOrElse((-1, -1))
+    val colMin = blocks.map(_.col).min
+    val colOffset = if (colMin < 0) abs(colMin) else 0
+    val rowMin = blocks.map(_.row).min
+    val rowOffset = if (rowMin < 0) abs(rowMin) else 0
+    val adjustedBlocks = blocks.map(b => b.copy(row = b.row + rowOffset, col = b.col + colOffset))
+    adjustedBlocks.map(b => {
+      b.digPlan.direction match
+        case 'R' => b.digPlan.steps
+        case 'D' => b.digPlan.steps * (b.col + 1)
+        case 'U' => b.digPlan.steps * -b.col
+        case _ => 0
 
-    val stack = mutable.Stack(startingPoint)
-    while (stack.nonEmpty) {
-      val (r, c) = stack.pop()
-      Seq((r - 1, c - 1), (r - 1, c), (r - 1, c + 1), (r, c - 1), (r, c + 1), (r + 1, c - 1), (r + 1, c), (r + 1, c + 1)).foreach({ case (r, c) =>
-        pool(r)(c) match
-          case '.' =>
-            stack.push((r, c))
-            pool(r)(c) = '#'
-          case _ =>
-      })
-    }
-    pool.flatten.count(_ == '#')
+    }).sum + 1
   }
 
-  def problem1(filePath: String): Int = {
+  def problem1(filePath: String): Long = {
     val input = readFile(filePath)
     buildLavaPool(input)
   }
 
   private def getDirection: DigPlan => Char = _.color.stripSuffix(")").last match
-      case '0' => 'R'
-      case '1' => 'D'
-      case '2' => 'L'
-      case '3' => 'U'
+    case '0' => 'R'
+    case '1' => 'D'
+    case '2' => 'L'
+    case '3' => 'U'
 
-  private def getSteps(d: DigPlan) = Integer.parseInt(d.color.substring(2, 7), 16)
+  private def getSteps(d: DigPlan) = java.lang.Long.parseLong(d.color.substring(2, 7), 16)
 
 
-  def problem2(filePath: String): Int = {
+  def problem2(filePath: String): Long = {
     val input = readFile(filePath)
     val updatedInput = input.map(d => d.copy(direction = getDirection(d), steps = getSteps(d)))
     buildLavaPool(updatedInput)
--- Day18/src/test/scala/problem/LavaductLagoonSpec.scala
+++ Day18/src/test/scala/problem/LavaductLagoonSpec.scala
@@ -7,13 +7,13 @@ import org.scalatest.matchers.should.Matchers
 
 class LavaductLagoonSpec extends AnyFlatSpec with GivenWhenThen with Matchers {
 
-  val filePath = "Day18/src/test/resources/input/input.XSCORE1.txt"
+  val filePath = "Day18/src/test/resources/input/input.XSCORE.txt"
 
-  "LavaductLagoon" should "match 59560" in {
-    LavaductLagoon.problem1(filePath) should be(59560)
+  "LavaductLagoon" should "match 36807" in {
+    LavaductLagoon.problem1(filePath) should be(36807)
   }
 
-  it should "match 69110" in {
-    LavaductLagoon.problem2(filePath) should be(69110)
+  it should "match 48797603984357" in {
+    LavaductLagoon.problem2(filePath) should be(48797603984357L)
   }
 }
