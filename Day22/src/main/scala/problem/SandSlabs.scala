dc883b08872756a82c15ae78e6c3597e7babd005
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2023-12-23 16:46:21 +0530

    Day22 part1 code cleanup.

--- Day22/src/main/scala/problem/SandSlabs.scala
+++ Day22/src/main/scala/problem/SandSlabs.scala
@@ -8,17 +8,18 @@ object SandSlabs {
 
   final case class Coordinate(x: Int, y: Int, z: Int)
 
-  final case class Brick(num: Int, start: Coordinate, end: Coordinate)
+  final case class Brick(num: Int, start: Coordinate, end: Coordinate, pairs: Set[(Int, Int)])
 
   final case class Support(supporting: Seq[Brick], supportedBy: Seq[Brick])
-  
+
+  private def createPairs(start: Coordinate, end: Coordinate) = Range.inclusive(start.x, end.x).flatMap(x => Range.inclusive(start.y, end.y).map(y => (x, y))).toSet
+
   private def readFile(filePath: String) = Using(Source.fromFile(filePath)) { file =>
     file
       .getLines().map(_.split('~').map(_.split(',') match
         case Array(x, y, z) => Coordinate(x.toInt, y.toInt, z.toInt)
       ) match
-        case Array(start, end) =>
-          Brick(0, start, end)
+        case Array(start, end) => Brick(0, start, end, createPairs(start, end))
       ).toSeq.sortBy(b => (b.start.z, b.start.x, b.start.y)).zipWithIndex.map(bi => bi._1.copy(num = bi._2 + 1))
   }.get
 
@@ -29,14 +30,10 @@ object SandSlabs {
       var movedBrick = elem
       breakable {
         for e <- elem.start.z - 1 to 1 by -1 do {
-          val myCoordinates = Range.inclusive(movedBrick.start.x, movedBrick.end.x).flatMap(x => Range.inclusive(movedBrick.start.y, movedBrick.end.y).map(y =>
-            (x, y))).toSet
-          if (!sum.exists(o => {
-            val otherCoordinates = Range.inclusive(o.start.x, o.end.x).flatMap(x => Range.inclusive(o.start.y, o.end.y).map(y =>
-              (x, y))).toSet
-            movedBrick.start.z - 1 == o.end.z && (myCoordinates intersect otherCoordinates).nonEmpty
-          })) {
-            movedBrick = movedBrick.copy(start = movedBrick.start.copy(z = e), movedBrick.end.copy(z = e + movedBrick.end.z - movedBrick.start.z))
+          if (!sum.exists(o => movedBrick.start.z - 1 == o.end.z && (movedBrick.pairs intersect o.pairs).nonEmpty)) {
+            val newStart = movedBrick.start.copy(z = e)
+            val newEnd = movedBrick.end.copy(z = e + movedBrick.end.z - movedBrick.start.z)
+            movedBrick = movedBrick.copy(start = newStart, newEnd, createPairs(newStart, newEnd))
           }
           else {
             break
@@ -46,27 +43,11 @@ object SandSlabs {
       sum :+ movedBrick
     })
     val brickSupportMap = movedBricks.map(b => {
-      val movedBrick = b
-      val myCoordinates = Range.inclusive(movedBrick.start.x, movedBrick.end.x).flatMap(x => Range.inclusive(movedBrick.start.y, movedBrick.end.y).map(y =>
-        (x, y))).toSet
-      b -> Support(movedBricks.filter(o => {
-        val otherCoordinates = Range.inclusive(o.start.x, o.end.x).flatMap(x => Range.inclusive(o.start.y, o.end.y).map(y =>
-          (x, y))).toSet
-        o != b && (myCoordinates intersect otherCoordinates).nonEmpty && b.end.z + 1 == o.start.z
-      }), // supporting
-        movedBricks.filter(o => {
-          val otherCoordinates = Range.inclusive(o.start.x, o.end.x).flatMap(x => Range.inclusive(o.start.y, o.end.y).map(y =>
-            (x, y))).toSet
-          o != b && (myCoordinates intersect otherCoordinates).nonEmpty &&
-            b.start.z - 1 == o.end.z
-        }) // supportedBy
+      b -> Support(movedBricks.filter(o => o != b && (b.pairs intersect o.pairs).nonEmpty && b.end.z + 1 == o.start.z), // supporting
+        movedBricks.filter(o => o != b && (b.pairs intersect o.pairs).nonEmpty && b.start.z - 1 == o.end.z) // supportedBy
       )
     }).toMap
-    brickSupportMap.count { case (_, s) =>
-      val check = !s.supporting.exists(b =>
-        brickSupportMap(b).supportedBy.size == 1)
-      check
-    }
+    brickSupportMap.count { case (_, s) => !s.supporting.exists(b => brickSupportMap(b).supportedBy.size == 1) }
   }
 
   def problem2(filePath: String): Int = {
