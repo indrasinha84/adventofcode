53e76d9bc3b2d12b132b48183de873e3ef2127cb
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2023-12-26 03:27:47 +0530

    Day 23 completed.

--- Day23/src/main/scala/problem/ALongWalk.scala
+++ Day23/src/main/scala/problem/ALongWalk.scala
@@ -4,11 +4,16 @@ import scala.io.Source
 import scala.util.Using
 import utils.{findNeighbours, findValidNeighboursF, isValidIndexF}
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 
 final case class Node(row: Int, col: Int)
 
-final case class Path(previous: Set[Node], distance: Int)
+final case class Edge(distance: Int, endNode: Node)
+
+final case class Vertice(node: Node, adjoiningNodes: Set[Edge])
+
+final case class Path(previous: Node, distance: Int)
 
 object ALongWalk {
 
@@ -27,6 +32,8 @@ object ALongWalk {
     ).flatten
   }
 
+  implicit def orderingByName[A <: Node]: Ordering[A] =
+    Ordering.by(e => (e.row, e.col))
 
   def problem1(filePath: String): Int = {
     val input = readFile(filePath)
@@ -37,27 +44,23 @@ object ALongWalk {
     var changesDone = true
     val validNodes = input.indices.flatMap(row => input(row).indices.map(col => Node(row, col)))
       .filter(n => input(n.row)(n.col) != '#')
-
-    implicit def orderingByName[A <: Node]: Ordering[A] =
-      Ordering.by(e => (e.row, e.col))
-
-    val pathMap = mutable.SortedMap.from(validNodes.map(n => n -> (if (n == start) Path(Set.empty, 0) else Path(Set.empty, Int.MaxValue))))
+    val pathMap = mutable.SortedMap.from(validNodes.map(n => n -> (if (n == start) Path(null, 0) else Path(null, Int.MaxValue))))
     while (changesDone) {
       changesDone = false
       pathMap.filter(_._1 != start).foreach(p => {
         val immediateNeighbours = pathFinder(p._1)
           .filter(n => pathMap(n).distance != Int.MaxValue)
-          .filter(n => n == start || (pathMap(n).previous - p._1).nonEmpty)
+          .filter(n => n == start || (pathMap(n).previous != p._1))
         immediateNeighbours.foreach(n => {
           val curr = pathMap(p._1)
           val prev = pathMap(n)
           if (curr.distance == Int.MaxValue || curr.distance > prev.distance - 1) {
-            pathMap(p._1) = Path(previous = Set(n), distance = prev.distance - 1)
+            pathMap(p._1) = Path(previous = n, distance = prev.distance - 1)
             changesDone = true
           }
-          else if (curr.distance == prev.distance - 1 && !curr.previous.contains(n)) {
+          else if (curr.distance == prev.distance - 1 && curr.previous != n) {
             changesDone = true
-            pathMap(p._1) = Path(previous = curr.previous + n, distance = prev.distance - 1)
+            pathMap(p._1) = Path(previous = n, distance = prev.distance - 1)
           }
 
         })
@@ -67,7 +70,54 @@ object ALongWalk {
   }
 
   def problem2(filePath: String): Int = {
-    val input = readFile(filePath)
-    0
+    val input = readFile(filePath).map(_.map(_.toString.replace('>', '.').replace('<', '.')
+      .replace('^', '.').replace('v', '.').head))
+    val start = Node(0, input.head.indexOf('.'))
+    val end = Node(input.length - 1, input.last.lastIndexOf('.'))
+    val isValidIndex = isValidIndexF(input.length, input.head.length)
+    val pathFinder = findPath(input, isValidIndex)
+    val validNodes = input.indices.flatMap(row => input(row).indices.map(col => Node(row, col)))
+      .filter(n => input(n.row)(n.col) != '#')
+    val multiNodes = Set(start, end) ++ validNodes.filter(n => pathFinder(n).size > 2).toSet
+
+    @tailrec
+    def addTillNextVertice(curr: Node, last: Node, edge: Edge): Edge = {
+      val nextNodes = pathFinder(curr).toSet - last
+      if ((nextNodes intersect multiNodes).nonEmpty) {
+        edge.copy(distance = edge.distance + 1, endNode = nextNodes.head)
+      }
+      else {
+        addTillNextVertice(nextNodes.head, curr, edge.copy(distance = edge.distance + 1, endNode = nextNodes.head))
+      }
+
+    }
+
+
+    val vertices = multiNodes.toSeq.sortBy(n => (n.row, n.col)).map(v => {
+      val edges = pathFinder(v).map(n => addTillNextVertice(n, v, Edge(1, n))).toSet
+      Vertice(node = v, adjoiningNodes = edges)
+    })
+    val verticesMap = vertices.map(v => v.node -> v).toMap
+
+
+    val unvisitedNodes: Set[Vertice] = vertices.toSet
+
+    var distance = 0
+
+    def findLongestPath(current: Vertice, unvisited: Set[Vertice], cnt: Int): Unit = {
+      if (current == verticesMap(start)) {
+        if (distance < cnt) {
+          distance = cnt
+          println(distance)
+        }
+      }
+      else {
+        current.adjoiningNodes.filter(a => unvisited.map(_.node).contains(a.endNode)).foreach(adj => {
+          findLongestPath(verticesMap(adj.endNode), unvisited - current, cnt + adj.distance)
+        })
+      }
+    }
+    findLongestPath(verticesMap(end), unvisitedNodes, 0)
+    distance
   }
 }
---
+++ common/src/main/scala/extensions/StringExtensions.scala
@@ -0,0 +1,22 @@
+package extensions
+
+object StringExtensions {
+
+
+  extension (str: String) {
+    def lowestAdventDigit(digits: Map[String, Int]): Int = {
+      digits.foldLeft((str, 0))((remainig, d) => {
+        val index = remainig._1.indexOf(d._1)
+        if (index > -1) (remainig._1.substring(0, index + d._1.length), d._2) else remainig
+      })._2
+    }
+
+    def highestAdventDigit(digits: Map[String, Int]): Int = {
+      digits.foldLeft((str, 0))((remainig, d) => {
+        val index = remainig._1.lastIndexOf(d._1)
+        if (index > -1) (remainig._1.substring(index), d._2) else remainig
+      })._2
+    }
+  }
+
+}
