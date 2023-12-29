6c9cad9d5303cf59735aed3c9c37323df258dbe0
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2023-12-29 06:40:09 +0530

    Day 11.

--- Day11/src/main/scala/problem/CosmicExpansion.scala
+++ Day11/src/main/scala/problem/CosmicExpansion.scala
@@ -18,6 +18,15 @@ object CosmicExpansion {
     input.flatMap(r => if (r.contains('#')) Seq(r) else Seq(r, r)).transpose.flatMap(r => if (r.contains('#')) Seq(r) else Seq(r, r)).transpose
   }
 
+  def findEmptyRows(input: Seq[Seq[Char]]): Set[Long] = {
+    input.zipWithIndex.filterNot(_._1.contains('#')).map(_._2.toLong).toSet
+  }
+
+
+  def findEmptyCols(input: Seq[Seq[Char]]): Set[Long] = {
+    input.transpose.zipWithIndex.filterNot(_._1.contains('#')).map(_._2.toLong).toSet
+  }
+
   private def numberTheGalaxies(input: Seq[Seq[Char]]): Seq[(Int, Node)] = {
     import extensions.SeqExtensions.*
     input.toGraphNodes.foldLeft((Seq.empty[(Int, Node)], 0))((sum, elem) => {
@@ -35,8 +44,18 @@ object CosmicExpansion {
       }).sum
   }
 
-  def problem2(filePath: String): Int = {
+  def problem2(filePath: String): Long = {
     val input = readFile(filePath)
-    0
+    val numberedGalaxies = numberTheGalaxies(input)
+    val emptyRows = findEmptyRows(input)
+    val emptyCols = findEmptyCols(input)
+    val multiplier = 1000000L
+    numberedGalaxies.combinations(2)
+      .map({
+        case Seq((_, source), (_, destination)) =>
+          val emptyRowsForThisPair = emptyRows.count(r => r < Math.max(source.row, destination.row) && r > Math.min(source.row, destination.row)) * (multiplier - 1)
+          val emptyColsForThisPair = emptyCols.count(r => r < Math.max(source.col, destination.col) && r > Math.min(source.col, destination.col)) *  (multiplier - 1)
+          (source.col - destination.col).abs + (source.row - destination.row).abs + emptyRowsForThisPair + emptyColsForThisPair
+      }).sum
   }
 }
--- Day11/src/test/scala/problem/CosmicExpansionSpec.scala
+++ Day11/src/test/scala/problem/CosmicExpansionSpec.scala
@@ -5,15 +5,15 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class NewProblemSpec extends AnyFlatSpec with GivenWhenThen with Matchers {
+class CosmicExpansionSpec extends AnyFlatSpec with GivenWhenThen with Matchers {
 
-  val filePath = "Day2/src/test/resources/input/input.XSCORE.txt"
+  val filePath = "Day11/src/test/resources/input/input.XSCORE.txt"
 
-//  "NewProblem" should "match 2810" in {
-//    Day3Problem.problem1(filePath) should be(2810)
-//  }
-//
-//  it should "match 69110" in {
-//    Day3Problem.problem2(filePath) should be(69110)
-//  }
+  "CosmicExpansion" should "match 9543156" in {
+    CosmicExpansion.problem1(filePath) should be(9543156)
+  }
+
+  it should "match 625243292686" in {
+    CosmicExpansion.problem2(filePath) should be(625243292686L)
+  }
 }
