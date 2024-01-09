934cc03a6ac2712fb3a234498c0a085356d00a3f
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2024-01-09 17:15:48 +0530

    Day 24 cleanup.

--- Day24/src/main/scala/problem/NeverTellMeTheOdds.scala
+++ Day24/src/main/scala/problem/NeverTellMeTheOdds.scala
@@ -1,10 +1,10 @@
 package problem
 
-import java.text.DecimalFormat
+import extensions.StringExtensions.*
+
 import scala.io.Source
 import scala.math.*
 import scala.util.Using
-import extensions.StringExtensions.*
 
 final case class Axis(x: BigDecimal, y: BigDecimal, z: BigDecimal)
 
@@ -24,16 +24,15 @@ object NeverTellMeTheOdds {
   }.get
 
 
-  def isRightDirection(p: BigDecimal, v: BigDecimal, newP: BigDecimal) = {
+  private def isRightDirection(p: BigDecimal, v: BigDecimal, newP: BigDecimal) = {
     (v.sign == -1 && newP < p) || (v.sign == 0 && newP == p) || (v.sign == 1 && newP > p)
   }
 
   private def findCrossingHailStorms(input: Seq[Hail], minRange: BigDecimal, maxRange: BigDecimal) = {
 
-    input.combinations(2).map(combination => {
+    input.combinations(2).count(combination => {
       val nodeFrom = combination.minBy(c => (c.position.x, c.position.y))
       val nodeTo = combination.maxBy(c => (c.position.x, c.position.y))
-
       val px1 = nodeFrom.position.x
       val py1 = nodeFrom.position.y
       val vx1 = nodeFrom.velocity.x
@@ -42,39 +41,19 @@ object NeverTellMeTheOdds {
       val py2 = nodeTo.position.y
       val vx2 = nodeTo.velocity.x
       val vy2 = nodeTo.velocity.y
-
       val c1 = px1 * vy1 - py1 * vx1
       val c2 = px2 * vy2 - py2 * vx2
-
-      if ((vy1 * vx2 - vx1 * vy2) != 0) {
-        val y = (vy2 * c1 - vy1 * c2) / (vy1 * vx2 - vx1 * vy2)
-        if (vy1 != 0) {
-          val x = (c1 + vx1 * y) / vy1
-          if (x >= minRange && x <= maxRange && y >= minRange && y <= maxRange) {
-            if (isRightDirection(px1, vx1, x) && isRightDirection(py1, vy1, y) && isRightDirection(px2, vx2, x) && isRightDirection(py2, vy2, y)) {
-              1
-            }
-            else {
-              0
-            }
-          }
-          else {
-            0
-          }
+      Option.when((vy1 * vx2 - vx1 * vy2) != 0 && vy1 != 0)((vy2 * c1 - vy1 * c2) / (vy1 * vx2 - vx1 * vy2)).map(y => ((c1 + vx1 * y) / vy1, y))
+        .exists { case (x, y) => x >= minRange && x <= maxRange && y >= minRange && y <= maxRange &&
+          isRightDirection(px1, vx1, x) && isRightDirection(py1, vy1, y) && isRightDirection(px2, vx2, x) && isRightDirection(py2, vy2, y)
         }
-        else {
-          0
-        }
-      }
-      else {
-        0
-      }
-    }).sum
+    }
+    )
   }
 
   def problem1(filePath: String): Int = {
     val input = readFile(filePath)
-    findCrossingHailStorms(input, "200000000000000".toBD, "400000000000000".toBD)
+    findCrossingHailStorms(input, BigDecimal.valueOf(200000000000000.0), BigDecimal.valueOf(400000000000000.0))
   }
 
   def problem2(filePath: String): Int = {
