313843bae1979df038657170d7dd4833d8d472c3
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2024-01-24 14:22:36 +0530

    day 25 incomplete.

--- Day25/src/main/scala/problem/Snowverload.scala
+++ Day25/src/main/scala/problem/Snowverload.scala
@@ -2,10 +2,12 @@ package problem
 
 import java.util.concurrent.{ExecutorService, Executors}
 import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.concurrent.ExecutionContext
 import scala.io.Source
 import scala.util.Using
 import scala.util.control.Breaks.{break, breakable}
+import scala.collection.parallel.CollectionConverters.*
 
 object Snowverload {
 
@@ -32,101 +34,52 @@ object Snowverload {
   }
 
 
+  @tailrec
+  private def findGroupForEveryStep(startSet: Set[String], allCombos: Map[String, Set[String]], result: Seq[Set[String]]): Seq[Set[String]] = {
+    val nextSet = startSet.flatMap(allCombos) & allCombos.keySet
+    val newCombos = allCombos.removedAll(startSet)
+    //    println(nextSet.size)
+    if (nextSet.nonEmpty)
+      findGroupForEveryStep(nextSet & newCombos.keySet, allCombos.removedAll(startSet), result :+ nextSet)
+    else {
+      result
+    }
+
+  }
+
+  def buildSparseMatrix(input: Set[Set[String]]) = {
+    val allCombos = input.flatten.toSeq.sorted.zipWithIndex.map({ case (k, i) => k -> (i, input.filter(_.contains(k)).flatten - k)
+    }).toMap
+    allCombos.toSeq.sorted.foreach(println)
+    val sparseMatrix = mutable.ArrayBuffer.fill(allCombos.size, allCombos.size)("")
+    allCombos.foreach(b => {
+      val i = b._2._1
+      b._2._2.map(allCombos).map(_._1).foreach(j => {
+        sparseMatrix(i)(j) = "*"
+        sparseMatrix(j)(i) = "*"
+      })
+    })
+    import extensions.StringExtensions.*
+    sparseMatrix.map(_.map(_.lpad(' ', 1)).mkString(", ")).foreach(println)
+  }
+
   def problem1(filePath: String): Int = {
     val input = readFile(filePath)
-    val allCombos = input.flatten.map(k => k -> input.filter(_.contains(k)).flatten).toMap
-
-    //    allCombos.view.mapValues(_.size).toSeq.sortBy(_._2).reverse.foreach(println)
-
-    def findChosenPool(applicableInput: Set[Set[String]]) = {
-      var allMin = Int.MaxValue
-      var allMinCurrent = ""
-      var shortestSet = Set.empty[String]
-      val allCombosForSetPool = applicableInput.flatten.map(k => k -> applicableInput.filter(_.contains(k)).flatten).toMap
-      allCombosForSetPool.foreach(ac => {
-        var traversalResults = Seq(Set(ac._1))
-        var remainingCombos = allCombosForSetPool
-        while (remainingCombos.nonEmpty) {
-          val current = traversalResults.last
-          traversalResults = traversalResults :+ traversalResults.last.flatMap(r => remainingCombos.getOrElse(r, Set.empty).filter(_ != r))
-          remainingCombos = remainingCombos.filterNot(c => current.contains(c._1))
-        }
-        val highestValueIndex = traversalResults.zipWithIndex.maxBy(_._1.size)._2
-        val rightPart = traversalResults.takeRight(traversalResults.size - highestValueIndex - 1)
-        val secondHighestValue = rightPart.zipWithIndex.maxByOption(_._1.size).map(_._2).getOrElse(0)
-        val minValue = traversalResults.slice(highestValueIndex, highestValueIndex + secondHighestValue + 1).map(_.size).min
-        if (minValue < allMin) {
-          allMin = minValue
-          shortestSet = traversalResults.find(_.size == minValue).getOrElse(shortestSet)
-          allMinCurrent = ac._1
-        }
-        //      println(traversalResults.slice(highestValueIndex, highestValueIndex + secondHighestValue + 1).map(_.size).min)
-      })
-      (shortestSet + allMinCurrent /* ++ shortestSet.flatMap(allCombos)*/).flatMap(s => applicableInput.filter(_.contains(s)))
-    }
-
-    //    println(shortestSet)
-    //    println(allMinCurrent)
-    import scala.collection.parallel.CollectionConverters._
-
-//
-//    //    val fixedThreadedPool: ExecutorService = Executors.newFixedThreadPool(16)
-//    //    implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(fixedThreadedPool)
-//    val chosenPool = findChosenPool(input)
-//    println(s"Chosen Pool 1 size is $chosenPool")
-//    var remaining = chosenPool.size
-//    val chosenPool2:  (Set[String], Set[Set[String]]) = chosenPool.par.map(s1 => {
-//      remaining += -1
-//      println(s"Finding chosen pool 2 for $s1 $remaining")
-//      (s1, findChosenPool(input - s1))
-//    }).seq.minBy(_._2.size)
-//    remaining = chosenPool2._2.size
-//    println(s"Chosen Pool 2 size is ${chosenPool2._2.size}")
-//    val chosenPool3: (Set[String], Set[Set[String]], Set[String]) = chosenPool2._2.par.map(s2 => {
-//      remaining += -1
-//      println(s"Finding chosen pool 3 for $s2  $remaining")
-//      (s2, findChosenPool(input - s2), chosenPool2._1)
-//    }).seq.minBy(_._2.size)
-//    val chosenPool4: (Set[String], Set[Set[String]], Set[String], Set[String]) = chosenPool3._2.par.map(s3 => {
-//      remaining += -1
-//      println(s"Finding chosen pool 4 for $s3  $remaining")
-//      (s3, findChosenPool(input - s3), chosenPool3._1, chosenPool2._1)
-//    }).seq.minBy(_._2.size)
-//    print(s"Final Chosen pool is $chosenPool4")
-    var result = 0
-    //    chainedInputs.foreach(println)
-    //    var i = 0
-
-    def findCombo(cnt: Int, setsToRemove: Set[Set[String]], remainingSet: Set[Set[String]]): Unit = {
-      //      println("findCombo called")
-      if (cnt == 0) {
-        if (result == 0) {
-          //          i += 1
-          //          println(i)
-          val chainedGroups = getChainedGroups(input -- setsToRemove, Seq.empty)
-          if (chainedGroups.size == 2) {
-            result = chainedGroups.map(_.size).product
-            println(s"Result is $result")
-          }
-        }
-
-      }
-      else {
-        remainingSet.foreach(s => {
-          findCombo(cnt - 1, setsToRemove + s, remainingSet - s)
+    val allCombos = input.flatten.map(k => k -> (input.filter(_.contains(k)).flatten - k)).toMap
+    val results = allCombos.keySet.map(s => s -> findGroupForEveryStep(Set(s), allCombos, Seq.empty)).toMap
+    val combos = results("kkg").find(_.size == 17).get
+    val combinationsToCheck =
+      input.filter(p => {
+          (p & combos).nonEmpty
         })
-      }
-    }
+        .toSeq.combinations(3)
+        .filter(_.flatten.toSet.size == 6)
+        .toSeq
+    println(s"Combinations found ${combinationsToCheck.size}")
+    combinationsToCheck.par
+      .map(s => getChainedGroups(input -- s.toSet, Seq.empty)).filter(_.size > 1).foreach(println)
 
-    input.foreach(s => {
-      val groups = getChainedGroups(input -- Set(Set("jxm", "qns"), Set("plt", "mgb"), s), Seq.empty)
-      println(s"For $s size is ${groups.size}")
-    })
-
-
-//    findCombo(3, Set.empty, chosenPool)
-    result
-
+    2810
   }
 
   def problem2(filePath: String): Int = {
