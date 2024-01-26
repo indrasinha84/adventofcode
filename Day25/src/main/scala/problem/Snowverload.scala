9eb75e47be5ffe9e8fd1b2da0b2afde3c97864c9
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2024-01-26 18:36:56 +0530

    day 25 incomplete.

--- Day25/src/main/scala/problem/Snowverload.scala
+++ Day25/src/main/scala/problem/Snowverload.scala
@@ -3,22 +3,10 @@ package problem
 import extensions.StringExtensions.*
 
 import scala.annotation.tailrec
-import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 import scala.util.{Random, Using}
 
-final case class Edge(distance: Int, endNode: String)
-
-final case class Vertice(name: String, adjoiningNodes: Set[String])
-
 object Snowverload {
-  //
-  //  extension (listOfList: Seq[_]) {
-  //    def toGraphNodes: Seq[String] = {
-  //      listOfList.indices.map(col => Node(row, col))
-  //    }
-  //  }
 
   private def readFile(filePath: String) = Using(Source.fromFile(filePath)) { file =>
     file.getLines().map(_.split(':') match
@@ -28,7 +16,6 @@ object Snowverload {
 
   @tailrec
   def getChainedGroups(input: Set[Set[String]], chain: Seq[Set[String]]): Seq[Set[String]] = {
-    //    println("getChainedGroups called")
     if (input.isEmpty) {
       chain
     }
@@ -47,7 +34,6 @@ object Snowverload {
   private def findGroupForEveryStep(startSet: Set[String], allCombos: Map[String, Set[String]], result: Seq[Set[String]]): Seq[Set[String]] = {
     val nextSet = startSet.flatMap(allCombos) & allCombos.keySet
     val newCombos = allCombos.removedAll(startSet)
-    //    println(nextSet.size)
     if (nextSet.nonEmpty)
       findGroupForEveryStep(nextSet & newCombos.keySet, allCombos.removedAll(startSet), result :+ nextSet)
     else {
@@ -56,64 +42,86 @@ object Snowverload {
 
   }
 
-  private def buildAdjacencyMatrix(input: Set[Set[String]]) = {
-    val allCombos = input.flatten.toSeq.sorted.zipWithIndex.map({ case (k, i) => k -> (i, input.filter(_.contains(k)).flatten - k) }).toMap
-    val adjacencyMatrix = mutable.ArrayBuffer.fill(allCombos.size, allCombos.size)(0)
-    allCombos.foreach(b => {
-      val i = b._2._1
-      b._2._2.map(allCombos).map(_._1).foreach(j => {
-        adjacencyMatrix(i)(j) = 1
-      })
-    })
-    adjacencyMatrix
+  final case class Graph(adj: Seq[Seq[Int]], verticesList: Seq[Set[String]])
+
+  private def buildGraph(input: Set[Set[String]]) = {
+    val allCombos = input.flatten.toSeq.sorted.zipWithIndex.map({ case (k, i) => k -> (i, input.filter(_.contains(k)).flatten - k) }).sortBy(_._2._1)
+    val verticesList = Seq.from(allCombos.map({ case (k, (_, _)) => Set(k) }))
+    val adjacencyMatrix = allCombos.map(b => allCombos.map(c => if (b._2._2.contains(c._1)) 1 else 0))
+    Graph(adjacencyMatrix, verticesList)
   }
 
-  private def contractMatrix(adj: ArrayBuffer[ArrayBuffer[Int]], from: Int, to: Int) = {
-    val adjCopy = adj.map(_.toArray).toArray
-    adjCopy(to).indices.foreach(j => {
-      if (j != from) {
-        adj(from)(j) += adj(to)(j)
-        adj(j)(from) += adj(j)(to)
-      }
+  private def contractMatrix(graph: Graph, from: Int, to: Int) = {
+    val newAdj = graph.adj.zipWithIndex.map({ case (row, i) =>
+      row.zipWithIndex.map({ case (item, j) =>
+        if (i == to) {
+          0
+        }
+        else if (j == to) {
+          0
+        }
+        else if (i == from && i != j) {
+          item + graph.adj(to)(j)
+        }
+        else if (j == from && i != j) {
+          item + graph.adj(i)(to)
+        }
+        else {
+          item
+        }
+      })
     })
-    adjCopy.indices.foreach(j => {
-      val newRow = adj(j)
-      newRow.remove(to)
-      adj(j) = newRow
-    })
-    adj.remove(to)
-    adj
+    val newVerticesList = graph.verticesList.zipWithIndex.map({ case (s, i) => if (i == from) s ++ graph.verticesList(to) else if (i == to) Set.empty else s })
+    graph.copy(adj = newAdj, verticesList = newVerticesList)
+  }
+
+  @tailrec
+  private def bisectGraph(graph: Graph): Graph = {
+    val rand = Random
+
+    @tailrec
+    def generateFrom: (Int, Seq[Int]) = {
+      val nonZeroFroms = graph.verticesList.zipWithIndex.filter(i => graph.verticesList(i._2).nonEmpty).map(_._2)
+      val from = rand.nextInt(graph.adj.length - 1)
+      val nonZeroTos = graph.adj.zipWithIndex.filter(i => i._2 != from && graph.adj(i._2)(from) > 0).map(_._2)
+      if (nonZeroFroms.contains(from) && nonZeroTos.nonEmpty) (from, nonZeroTos) else generateFrom
+    }
+
+    val (from, nonZeroTos) = generateFrom
+
+    @tailrec
+    def generateTo: Int = {
+      val nextNum = if (nonZeroTos.length > 1) rand.nextInt(nonZeroTos.length - 1) else 0
+//      if (nonZeroTos.isEmpty)
+//        println
+      val num = nonZeroTos(nextNum)
+      if (nonZeroTos.contains(num)) num else generateTo
+    }
+
+    val to = generateTo
+    val newGraph = contractMatrix(graph, from, to)
+    //    println(s"After contracting from $from to $to")
+    //    newGraph.adj.map(_.map(_.toString.lpad(' ', 1)).mkString(", ")).foreach(println)
+    if (newGraph.verticesList.count(_.nonEmpty) > 2) bisectGraph(newGraph) else newGraph
   }
 
   def problem1(filePath: String): Int = {
     val input = readFile(filePath)
     val inputSet = input.map(_.toSet).toSet
-    var adj = buildAdjacencyMatrix(inputSet)
-
-    while (adj.length > 2) {
-      adj.map(_.map(_.toString.lpad(' ', 1)).mkString(", ")).foreach(println)
+    val graph = buildGraph(inputSet)
+    var bisectedGraph = buildGraph(inputSet)
+//    bisectedGraph.adj.map(_.map(_.toString.lpad(' ', 1)).mkString(", ")).foreach(println)
+    //    bisectedGraph = bisectGraph(graph)
+//    println
+    //    bisectedGraph.adj.map(_.map(_.toString.lpad(' ', 1)).mkString(", ")).foreach(println)
+    while (bisectedGraph.adj.flatten.exists(_ != 3)) {
+      bisectedGraph = bisectGraph(graph)
+//      bisectedGraph.adj.map(_.map(_.toString.lpad(' ', 1)).mkString(", ")).foreach(println)
       println
-      val rand = Random
-      val from = rand.nextInt(adj.length - 1)
-      val nonZeros = adj(from).zipWithIndex.filter(_._1 > 0).map(_._2)
-
-      @tailrec
-      def generateTo: Int = {
-        val nextNum = if (nonZeros.length > 1) rand.nextInt(nonZeros.length - 1) else 0
-        val num = nonZeros(nextNum)
-        if (num != from) num else generateTo
-      }
-
-      val to = generateTo
-      adj = contractMatrix(adj, from, to)
     }
-    adj.map(_.map(_.toString.lpad(' ', 1)).mkString(", ")).foreach(println)
-
-    val verticesMap = inputSet.flatten.toSeq.sorted.zipWithIndex.map({ case (k, i) => k -> (i, inputSet.filter(_.contains(k)).flatten - k) })
-    val vertices = verticesMap.sortBy(_._2._1).map(v => {
-      Vertice(name = v._1, adjoiningNodes = v._2._2)
-    })
-    2810
+//    println
+//    graph.verticesList.foreach(println)
+    graph.verticesList.map(_.size).sum
   }
 
   def problem2(filePath: String): Int = {
